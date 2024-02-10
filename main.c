8c1852f1ac6f5a6c24f7ee787808de81eb99ac4f
m4jbz <zeroknel@proton.me>
2024-02-10 00:25:16 -0600

    second version

--- main.c
+++ main.c
@@ -4,25 +4,28 @@
 #include <time.h>
 #include <stdbool.h>
 
-// Numero de filas y columnas
-#define N 3
+// Numero de posiciones
+#define N 9
 
 // Declaración de las funciones
-char **boardMaker();
-char **newBoardMaker(char **board, int row, int file);
-void printBoard(char **board);
-void freeMatrix(char **board);
-bool youWin(char **board);
-bool pcWin(char **board);
-bool isValid(int row, int file, char **board);
+char *boardMaker();
+char *newBoardMaker(int randomNum, char *board, int position);
+void printBoard(char *board);
+void freeArray(char *board);
+bool youWin(char *board);
+bool pcWin(char *board);
+int validPst(int validPositions);
 void clean();
 
 // Función main
 int main() 
 {
-	int row, file;
-	char **board = boardMaker();
-	int quitGame = 0;
+	srand(time(0));
+
+	char *board = boardMaker();
+	int *allPositions = calloc(9, sizeof(int));
+	int position, randomNum;
+	int quitGame = 0, i = 0, j = 0;
 
 	clean();
 	printf("%*s%s\n", 67, "", "Juego del gato 🐈\n");
@@ -30,26 +33,40 @@ int main()
 
 	while (1)
 	{
-		printf("Posición [][]: \n");
-		scanf("%d", &row);
-		scanf("%d", &file);
+		printf("Posición []: \n");
+		scanf("%d", &position);
 
-		if (row > N || file > N || row < 1 || file < 1 || !isValid(row, file, board))
+		if (position > N || position < 1 || !(board[position-1] == ' '))
 		{
 			printf("Posición no valida. Intenta de nuevo.\n");
 		}
 		else 
 		{
-			do {
-				if (isValid(row, file, board))
-				{
-					board = newBoardMaker(board, row, file);
-					printf("%*s%s\n", 67, "", "Juego del gato 🐈\n");
-					printBoard(board);
-					break;
-				}
-			} while (isValid(row, file, board));
+			allPositions[i] = position-1;
+			i++;
+
+			if (i < 9)
+			{
+				do {
+					randomNum = rand() % 9;
+					j = 0;
+					while (j < 9)
+					{
+						if (randomNum == allPositions[j])
+							break;
+						j++;
+					}
+				} while (randomNum == allPositions[j]);
+				allPositions[i] = randomNum;
+			}
+
+			i++;
+			board = newBoardMaker(randomNum, board, position);
+			clean();
+			printf("%*s%s\n", 67, "", "Juego del gato 🐈\n");
+			printBoard(board);
 		}
+
 		if (youWin(board))
 		{
 			printf("Ganaste!\n");
@@ -60,122 +77,76 @@ int main()
 			printf("La PC Ganó!\n");
 			break;
 		}
+		if (!(youWin(board)) && !(pcWin(board)) && i == 10)
+		{
+			printf("Es un empate!\n");
+			break;
+		}
 	}
 
-	freeMatrix(board);
+	freeArray(board);
+	free(allPositions);
 	
 	return 0;
 }
 
-
-// Función que hace el primer tablero
-char **boardMaker()
-{
-	char **board = malloc(N * sizeof(char *));
-
-	for (int i = 0; i < N; i++)
-	{
-		board[i] = malloc(N * sizeof(char));
-
-		for (int j = 0; j < N; j++)
-		{
-			board[i][j] = ' ';
-		}
-	}
-
-	return board;
-}
-
 // Función para hacer un nuevo tablero modificado
-char **newBoardMaker(char **board, int row, int file)
+char *newBoardMaker(int randomNum, char *board, int position)
 {
-	srand(time(0));
-
-	int *arrRandom = malloc(2 * sizeof(int));
-	arrRandom[0] = rand() % 3;
-	arrRandom[1] = rand() % 3;
-
-	printf("PC: %d, %d\n", arrRandom[0], arrRandom[1]);
-	printf("User: %d, %d\n", row-1, file-1);
-
-	if (arrRandom[0] == (row-1) && arrRandom[1] == (file-1))
-	{
-		int offset1 = (arrRandom[0] > 1) ? -1 : 1;
-		int offset2 = (arrRandom[1] > 1) ? -1 : 1;
-
-		board[arrRandom[0]+offset1][arrRandom[1]+offset2] = 'X';
-	} 
-	else if (board[arrRandom[0]][arrRandom[1]] == 'O')
-	{
-		int offset1 = (arrRandom[0] > 1) ? -1 : 1;
-		int offset2 = (arrRandom[1] > 1) ? -1 : 1;
-
-		board[arrRandom[0]+offset1][arrRandom[1]+offset2] = 'X';
-	} 
-	else if (board[arrRandom[0]][arrRandom[1]] == 'X')
-	{
-		int offset1 = (arrRandom[0] > 1) ? -1 : 1;
-		int offset2 = (arrRandom[1] > 1) ? -1 : 1;
-
-		board[arrRandom[0]+offset1][arrRandom[1]+offset2] = 'X';
-	} 
-	else 
-	{
-		board[arrRandom[0]][arrRandom[1]] = 'X';
-	} 
-
-	board[row-1][file-1] = 'O';
-
-	free(arrRandom);
+	board[randomNum] = 'X';
+	board[position-1] = 'O';
 
 	return board;
 }
 
-bool isValid(int row, int file, char **board)
+// A partir de aca todo funciona correctamente
+// Función que hace el primer tablero
+char *boardMaker()
 {
+	char *board = malloc(N * sizeof(char));
+
 	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < N; j++)
-		{
-			if (board[row-1][file-1] == ' ')
-				return true;
-		}
+		board[i] = ' ';
 	}
-	return true;
+
+	return board;
 }
 
 // Función que imprime el tablero
-void printBoard(char **board) 
+void printBoard(char *board) 
 {
+	int k = 0;
 	printf("%*s|---|---|---|\n", 69, "");
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < 3; i++)
 	{
 		printf("%*s|", 69, "");
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j < 3; j++)
 		{
-			printf(" %c |", board[i][j]);
+			printf(" %c |", board[k]);
+			k++;
 		}
 		printf("\n");
 		printf("%*s|---|---|---|\n", 69, "");
 	}
 }
 
-bool youWin(char **board)
+bool youWin(char *board)
 {
-	if (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O' ||
-			board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O' ||
-			board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O')
+	if (board[0] == 'O' && board[1] == 'O' && board[2] == 'O' ||
+			board[3] == 'O' && board[4] == 'O' && board[5] == 'O' ||
+			board[6] == 'O' && board[7] == 'O' && board[8] == 'O')
 	{
 		return true;
 	}
-	else if (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O' ||
-					 board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O' ||
-					 board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O')
+	else if (board[0] == 'O' && board[3] == 'O' && board[6] == 'O' ||
+					 board[1] == 'O' && board[4] == 'O' && board[7] == 'O' ||
+					 board[2] == 'O' && board[5] == 'O' && board[8] == 'O')
 	{
 		return true;
 	}
-	else if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O' ||
-					 board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O')
+	else if (board[0] == 'O' && board[4] == 'O' && board[8] == 'O' ||
+					 board[2] == 'O' && board[4] == 'O' && board[6] == 'O')
 	{
 		return true;
 	}
@@ -184,22 +155,22 @@ bool youWin(char **board)
 	}
 }
 
-bool pcWin(char **board)
+bool pcWin(char *board)
 {
-	if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X' ||
-			board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X' ||
-			board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X')
+	if (board[0] == 'X' && board[1] == 'X' && board[2] == 'X' ||
+			board[3] == 'X' && board[4] == 'X' && board[5] == 'X' ||
+			board[6] == 'X' && board[7] == 'X' && board[8] == 'X')
 	{
 		return true;
 	}
-	else if (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X' ||
-					 board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X' ||
-					 board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X')
+	else if (board[0] == 'X' && board[3] == 'X' && board[6] == 'X' ||
+					 board[1] == 'X' && board[4] == 'X' && board[7] == 'X' ||
+					 board[2] == 'X' && board[5] == 'X' && board[8] == 'X')
 	{
 		return true;
 	}
-	else if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X' ||
-					 board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X')
+	else if (board[0] == 'X' && board[4] == 'X' && board[8] == 'X' ||
+					 board[2] == 'X' && board[4] == 'X' && board[6] == 'x')
 	{
 		return true;
 	}
@@ -209,12 +180,8 @@ bool pcWin(char **board)
 }
 
 // Función para liberar memoria
-void freeMatrix(char **board)
+void freeArray(char *board)
 {
-	for (int i = 0; i < N; i++)
-	{
-		free(board[i]);
-	}
 	free(board);
 }
 
